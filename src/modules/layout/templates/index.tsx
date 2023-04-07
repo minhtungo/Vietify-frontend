9daa5a39e43e5588c542433602f4be95dcde30ce
Minh Tu Ngo <loducfc@gmail.com>
2023-04-07 00:20:48 -0400

    Upgrade dependencies and migrate to Next.js 13 on top of Medusa Storefront Starter

---
+++ src/modules/layout/templates/index.tsx
@@ -0,0 +1,27 @@
+import Footer from "@modules/layout/templates/footer"
+import Nav from "@modules/layout/templates/nav"
+import React from "react"
+
+import { Inter } from "next/font/google"
+import clsx from "clsx"
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["400", "600"],
+  display: "swap",
+})
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div>
+      <Nav />
+      <main className={clsx(inter.variable, "relative font-sans")}>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
