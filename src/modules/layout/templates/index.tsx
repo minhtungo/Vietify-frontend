e8fc244fe5df8583c448c6990fd32079bd24c1f0
Minh Tu Ngo <loducfc@gmail.com>
2023-04-09 01:47:00 -0400

    style: change 404 page style

--- src/modules/layout/templates/index.tsx
+++ src/modules/layout/templates/index.tsx
@@ -1,27 +1,31 @@
-import Footer from "@modules/layout/templates/footer"
-import Nav from "@modules/layout/templates/nav"
-import React from "react"
+import Footer from '@modules/layout/templates/footer';
+import Nav from '@modules/layout/templates/nav';
+import React from 'react';
 
-import { Inter } from "next/font/google"
-import clsx from "clsx"
+import { Inter } from 'next/font/google';
+import clsxm from '@lib/clsxm';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  weight: ["400", "600"],
-  display: "swap",
-})
+  subsets: ['latin'],
+  variable: '--font-inter',
+  weight: ['400', '600'],
+  display: 'swap',
+});
 
-const Layout: React.FC = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
       <Nav />
-      <main className={clsx(inter.variable, "relative font-sans")}>
+      <main className={clsxm(inter.variable, 'relative font-sans')}>
         {children}
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,28 +1,44 @@
-import Head from "@modules/common/components/head"
-import Layout from "@modules/layout/templates"
-import Link from "next/link"
-import { ReactElement } from "react"
-import { NextPageWithLayout } from "types/global"
+import Head from '@modules/common/components/head';
+import Layout from '@modules/layout/templates';
+import Link from 'next/link';
+import { ReactElement } from 'react';
+import { NextPageWithLayout } from 'types/global';
 
 const NotFound: NextPageWithLayout = () => {
-  return <>
-    <Head title="404" description="Something went wrong" />
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-gry-900">Page not found</h1>
-      <p className="text-small-regular text-gray-700">
-        The page you tried to access does not exist.
-      </p>
-      <Link href="/" className="mt-4 underline text-base-regular text-gray-900">
-        
-          Go to frontpage
-        
-      </Link>
-    </div>
-  </>;
-}
+  return (
+    <>
+      <Head title="404" description="Something went wrong" />
+      <main className="grid min-h-[calc(100vh-64px)] place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 ">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Page not found
+          </h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              href="/"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Go back home
+            </Link>
+            <Link
+              href="/contact"
+              className="text-sm font-semibold text-gray-900"
+            >
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
 
 NotFound.getLayout = (page: ReactElement) => {
-  return <Layout>{page}</Layout>
-}
+  return <Layout>{page}</Layout>;
+};
 
-export default NotFound
+export default NotFound;
