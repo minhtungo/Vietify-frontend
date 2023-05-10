777765c37f3d9571fab169d3ba315fcb87bc9fd4
Minh Tu Ngo <loducfc@gmail.com>
2023-05-10 14:46:01 -0400

    style: change style for auth forms

--- src/modules/account/components/login/index.tsx
+++ src/modules/account/components/login/index.tsx
@@ -11,6 +11,16 @@ import Button from '@ui//button';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@modules/ui/card';
+import Link from '@modules/common/components/link';
+import { Checkbox } from '@modules/ui/checkbox';
 
 interface SignInCredentials extends FieldValues {
   email: string;
@@ -44,72 +54,95 @@ const Login = () => {
   });
 
   return (
-    <div className="flex w-full max-w-md flex-col">
+    <>
       {isSubmitting && (
         <div className="fixed inset-0 z-10 flex items-center justify-center bg-white bg-opacity-50">
           <Spinner size={24} />
         </div>
       )}
-      <Heading variant="heading" className="mb-4 text-center">
-        Chào mừng đến với Vietify!
-      </Heading>
-
-      <form className="w-full" onSubmit={onSubmit}>
-        <div className="flex w-full flex-col gap-y-3.5">
-          <Input
-            label="Email"
-            {...register('email', { required: 'Email is required' })}
-            autoComplete="email"
-            errors={errors}
-          />
-          <Input
-            label="Mật khẩu"
-            {...register('password', { required: 'Password is required' })}
-            type="password"
-            autoComplete="current-password"
-            errors={errors}
-          />
-        </div>
-        {authError && (
-          <div className="text-small-regular w-full text-destructive">
-            These credentials do not match our records
+      <Card className={cn('max-w-sm')}>
+        <CardHeader className="text-center">
+          <CardTitle>Chào mừng đến với Vietify!</CardTitle>
+          {/* <CardDescription>You have 3 unread messages.</CardDescription> */}
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <form onSubmit={onSubmit} className="flex flex-col gap-2">
+            <Input
+              label="Email"
+              {...register('email', { required: 'Email is required' })}
+              autoComplete="email"
+              errors={errors}
+            />
+            <Input
+              label="Mật khẩu"
+              {...register('password', { required: 'Password is required' })}
+              type="password"
+              autoComplete="current-password"
+              errors={errors}
+            />
+            {authError && (
+              <div>
+                <span className="text-small-regular w-full text-rose-500">
+                  Incorrect email or password.
+                </span>
+              </div>
+            )}
+          </form>
+          <div className="flex justify-between">
+            <div className="flex items-center space-x-2">
+              <Checkbox id="remember" />
+              <label
+                htmlFor="remember"
+                className="text-[13px] font-medium text-foreground"
+              >
+                Nhớ mật khẩu
+              </label>
+            </div>
+            <Link
+              href="/terms-of-use"
+              className="text-[13px] text-brand hover:underline"
+            >
+              Quên mật khẩu?
+            </Link>
           </div>
-        )}
-        <Button className="mt-6 w-full">Đăng nhập</Button>
-      </form>
+          <Button className="mt-1 w-full">Đăng nhập</Button>
+          <div className="flex items-center">
+            <div className="h-[0.5px] flex-grow bg-border" />
+            <Text className="mx-3 text-[12px]">hoặc tiếp tục với</Text>
+            <div className="h-[0.5px] flex-grow bg-border" />
+          </div>
+          <div className="flex w-full justify-between gap-3">
+            <Button variant="outline" className="w-full">
+              <Google className="mr-2 h-4 w-4" />
+              Google
+            </Button>
+            <Button variant="outline" className="w-full">
+              <Facebook className="mr-2 h-4 w-4" />
+              Facebook
+            </Button>
+          </div>
+        </CardContent>
 
-      <div className="my-6 flex items-center">
-        <div className="h-[0.5px] flex-grow bg-border" />
-        <Text className="mx-3 text-[12px]">hoặc tiếp tục với</Text>
-        <div className="h-[0.5px] flex-grow bg-border" />
-      </div>
-
-      <div className="flex w-full justify-between gap-3">
-        <Button variant="outline" className="w-full">
-          <Google className="mr-2 h-4 w-4" />
-          Google
-        </Button>
-        <Button variant="outline" className="w-full">
-          <Facebook className="mr-2 h-4 w-4" />
-          Facebook
-        </Button>
-      </div>
-
-      <Text variant="info" className="mt-3 text-center text-[11px]">
-        {'Thành viên mới? '}
-        <button
-          onClick={() => setCurrentView(LOGIN_VIEW.REGISTER)}
-          className={cn(
-            textVariants({
-              variant: 'link',
-              className: '!text-[11px] underline',
-            })
-          )}
-        >
-          Đăng kí
-        </button>
-      </Text>
-    </div>
+        <CardFooter className="flex flex-col gap-3 text-center">
+          <Text variant="info" className="text-[11px]">
+            Bằng việc đăng nhập, bạn đã đồng ý với Vietify về{' '}
+            <Link href="/terms-of-use" className="text-brand hover:underline">
+              Điều khoản dịch vụ
+            </Link>
+            {' & '}
+            <Link href="/privacy-policy" className="text-brand hover:underline">
+              Chính sách bảo mật
+            </Link>
+          </Text>
+          <Text variant="info" className="text-[13px]">
+            Thành viên mới?{' '}
+            <Link href="/register" className="text-brand hover:underline">
+              Đăng kí
+            </Link>
+          </Text>
+        </CardFooter>
+      </Card>
+    </>
   );
 };
 
--- src/modules/account/components/register/index.tsx
+++ src/modules/account/components/register/index.tsx
@@ -3,6 +3,14 @@ import Spinner from '@icons/spinner';
 import { medusaClient } from '@lib/config';
 import { LOGIN_VIEW, useAccount } from '@lib/context/account-context';
 import cn from '@lib/util/cn';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@modules/ui/card';
 import Heading from '@modules/ui/heading';
 import Text, { textVariants } from '@modules/ui/text';
 import Button from '@ui/button';
@@ -46,97 +54,81 @@ const Register = () => {
   });
 
   return (
-    <div className="flex max-w-sm flex-col items-center">
+    <>
       {isSubmitting && (
         <div className="fixed inset-0 z-10 flex items-center justify-center bg-white bg-opacity-50">
           <Spinner size={24} />
         </div>
       )}
-      <Heading variant="heading" className="mb-4">
-        Tạo tài khoản
-      </Heading>
+      <Card className={cn('max-w-sm')}>
+        <CardHeader className="text-center">
+          <CardTitle>Tạo tài khoản</CardTitle>
+          {/* <CardDescription>You have 3 unread messages.</CardDescription> */}
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <form onSubmit={onSubmit} className="flex flex-col gap-2">
+            <Input
+              label="Họ"
+              {...register('last_name', {
+                required: 'Last name is required',
+              })}
+              autoComplete="family-name"
+              errors={errors}
+            />
+            <Input
+              label="Tên"
+              {...register('first_name', {
+                required: 'First name is required',
+              })}
+              autoComplete="given-name"
+              errors={errors}
+            />
+            <Input
+              label="Email"
+              {...register('email', { required: 'Email is required' })}
+              autoComplete="email"
+              errors={errors}
+            />
+            <Input
+              label="Mật khẩu"
+              {...register('password', {
+                required: 'Password is required',
+              })}
+              type="password"
+              autoComplete="new-password"
+              errors={errors}
+            />
 
-      <form className="flex w-full flex-col" onSubmit={onSubmit}>
-        <div className="flex w-full flex-col gap-y-2">
-          <Input
-            label="Họ"
-            {...register('last_name', { required: 'Last name is required' })}
-            autoComplete="family-name"
-            errors={errors}
-          />
-          <Input
-            label="Tên"
-            {...register('first_name', { required: 'First name is required' })}
-            autoComplete="given-name"
-            errors={errors}
-          />
-          <Input
-            label="Email"
-            {...register('email', { required: 'Email is required' })}
-            autoComplete="email"
-            errors={errors}
-          />
-          <Input
-            label="Mật khẩu"
-            {...register('password', {
-              required: 'Password is required',
-            })}
-            type="password"
-            autoComplete="new-password"
-            errors={errors}
-          />
-        </div>
-        {authError && (
-          <div>
-            <span className="text-small-regular w-full text-rose-500">
-              Incorrect email or password.
-            </span>
-          </div>
-        )}
-
-        <Button className="mt-6 w-full">Tạo tài khoản</Button>
-      </form>
-      <Text variant="info" className="mt-6 text-center text-[11px]">
-        Bằng việc đăng ký, bạn đã đồng ý với Vietify về{' '}
-        <Link
-          href="/terms-of-use"
-          className={cn(
-            textVariants({
-              variant: 'link',
-              className: '!text-[11px] underline',
-            })
-          )}
-        >
-          Điều khoản dịch vụ
-        </Link>
-        {' & '}
-        <Link
-          href="/privacy-policy"
-          className={cn(
-            textVariants({
-              variant: 'link',
-              className: '!text-[11px] underline',
-            })
-          )}
-        >
-          Chính sách bảo mật
-        </Link>
-      </Text>
-      <Text variant="info" className="mt-3 text-center text-[11px]">
-        Bạn đã là thành viên?{' '}
-        <button
-          onClick={() => setCurrentView(LOGIN_VIEW.SIGN_IN)}
-          className={cn(
-            textVariants({
-              variant: 'link',
-              className: '!text-[11px] underline',
-            })
-          )}
-        >
-          Đăng nhập
-        </button>
-      </Text>
-    </div>
+            {authError && (
+              <div>
+                <span className="text-small-regular w-full text-rose-500">
+                  Incorrect email or password.
+                </span>
+              </div>
+            )}
+          </form>
+          <Button className="mt-1 w-full">Tạo tài khoản</Button>
+        </CardContent>
+        <CardFooter className="flex flex-col gap-3 text-center">
+          <Text variant="info" className="text-[11px]">
+            Bằng việc đăng ký, bạn đã đồng ý với Vietify về{' '}
+            <Link href="/terms-of-use" className="text-brand hover:underline">
+              Điều khoản dịch vụ
+            </Link>
+            {' & '}
+            <Link href="/privacy-policy" className="text-brand hover:underline">
+              Chính sách bảo mật
+            </Link>
+          </Text>
+          <Text variant="info" className="text-[13px]">
+            Bạn đã là thành viên?{' '}
+            <Link href="/login" className="text-brand hover:underline">
+              Đăng nhập
+            </Link>
+          </Text>
+        </CardFooter>
+      </Card>
+    </>
   );
 };
 
