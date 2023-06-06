91a9774532562d2cf33c4478063f7b90e49def7f
Minh Tu Ngo <loducfc@gmail.com>
2023-06-06 01:30:07 -0400

    implement ssg

--- src/pages/shop/[handle].tsx
+++ src/pages/shop/[handle].tsx
@@ -1,33 +1,46 @@
 import Head from '@common/head';
-import { fetchCategoryProducts, fetchProductsList } from '@lib/data';
+import { medusaClient } from '@lib/config';
+import { IS_BROWSER } from '@lib/constants';
+import { fetchProductsList } from '@lib/data';
+import usePreviews from '@lib/hooks/use-previews';
 import useQueryParams from '@lib/hooks/use-query-params';
-import { getProductHandles } from '@lib/util/get-product-handles';
+import { getCategoryHandles } from '@lib/util/get-category-handles';
 import { StoreGetProductsParams } from '@medusajs/medusa';
-import { PricedProduct } from '@medusajs/medusa/dist/types/pricing';
 import Layout from '@modules/layout/templates';
-import ProductPreview from '@modules/products/components/product-preview';
-import Heading from '@modules/ui/heading';
-import Text from '@modules/ui/text';
-import { defaultSort, sorting } from '@static/sort-options';
+import InfiniteProducts from '@modules/products/components/infinite-products';
+import SkeletonCollectionPage from '@modules/skeletons/templates/skeleton-collection-page';
+import RefinementList from '@modules/store/components/refinement-list';
 import {
   QueryClient,
   dehydrate,
   useInfiniteQuery,
 } from '@tanstack/react-query';
-import { useCart, useProductCategories, useProducts } from 'medusa-react';
+import { useCart, useProductCategories } from 'medusa-react';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 import { ParsedUrlQuery } from 'querystring';
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, useState } from 'react';
 import { NextPageWithLayout, PrefetchedPageProps } from 'types/global';
-import RefinementList from '@modules/store/components/refinement-list';
-import InfiniteProducts from '@modules/products/components/infinite-products';
-import usePreviews from '@lib/hooks/use-previews';
-import Container from '@modules/layout/components/container';
 
 interface Params extends ParsedUrlQuery {
   handle: string;
 }
+
+const fetchCategoryIdsProducts = async (
+  categoryIds: string[],
+  pageParam = 0
+) => {
+  const { products, count, offset } = await medusaClient.products.list({
+    category_id: categoryIds,
+    limit: 12,
+    offset: pageParam,
+  });
+
+  return {
+    response: { products, count },
+    nextPage: count > offset + 12 ? offset + 12 : null,
+  };
+};
 
 const CategoryPage: NextPageWithLayout<PrefetchedPageProps> = ({
   notFound,
@@ -37,29 +50,45 @@ const CategoryPage: NextPageWithLayout<PrefetchedPageProps> = ({
 
   const { cart } = useCart();
 
-  const { query } = useRouter();
+  const { query, replace } = useRouter();
   const handle = typeof query.handle === 'string' ? query.handle : '';
-
-  const { sort } = query;
 
   const { product_categories: categories } = useProductCategories({ handle });
 
   const categoryIds = categories?.map((c) => c.id);
 
-  const { data, hasNextPage, fetchNextPage, isLoading, isFetchingNextPage } =
-    useInfiniteQuery(
-      [`fetch-category-products`, queryParams, cart],
-      ({ pageParam }) =>
-        fetchProductsList({ pageParam, queryParams, categoryIds }),
-      {
-        getNextPageParam: (lastPage) => lastPage.nextPage,
-      }
-    );
+  const {
+    data,
+    hasNextPage,
+    fetchNextPage,
+    isLoading,
+    isError,
+    isFetchingNextPage,
+  } = useInfiniteQuery(
+    [`fetch-category-products`, handle],
+    ({ pageParam }) =>
+      fetchProductsList({ pageParam, queryParams, categoryIds }),
+    {
+      getNextPageParam: (lastPage) => lastPage.nextPage,
+    }
+  );
 
   const products = usePreviews({
     pages: data?.pages,
     region: cart?.region,
   });
+
+  if (notFound) {
+    if (IS_BROWSER) {
+      replace('/404');
+    }
+
+    return <SkeletonCollectionPage />;
+  }
+
+  if (isError) {
+    replace('/404');
+  }
 
   return (
     <>
@@ -84,43 +113,49 @@ CategoryPage.getLayout = (page: ReactElement) => {
 
 export default CategoryPage;
 
-// const { products } = useProducts({
-//   category_id: categoryId ? [categoryId] : [],
-// });
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  const handles = await getCategoryHandles();
+  return {
+    paths: handles.map((handle) => ({ params: { handle } })),
+    fallback: true,
+  };
+};
 
-// export const getStaticPaths: GetStaticPaths<Params> = async () => {
-//   const handles = await getProductHandles();
-//   return {
-//     paths: handles.map((handle) => ({ params: { handle } })),
-//     fallback: true,
-//   };
-// };
+export const getStaticProps: GetStaticProps = async (context) => {
+  const queryClient = new QueryClient();
+  const handle = context.params?.handle as string;
 
-// export const getStaticProps: GetStaticProps = async (context) => {
-//   const queryClient = new QueryClient();
-//   const handle = context.params?.handle as string;
+  const categoryIds = await medusaClient.productCategories
+    .list({
+      handle,
+    })
+    .then(({ product_categories }) => product_categories.map((c) => c.id));
 
-//   await queryClient.prefetchQuery([`get_category_products`, handle], () =>
-//     fetchCategoryProducts({ handle })
-//   );
+  await queryClient.prefetchInfiniteQuery(
+    ['fetch-category-products', handle],
+    ({ pageParam }) => fetchCategoryIdsProducts(categoryIds, pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.nextPage,
+    }
+  );
 
-//   const queryData = await queryClient.getQueryData([
-//     `get_category_products`,
-//     handle,
-//   ]);
+  const queryData = await queryClient.getQueryData([
+    `fetch-category-products`,
+    handle,
+  ]);
 
-//   if (!queryData) {
-//     return {
-//       props: {
-//         notFound: true,
-//       },
-//     };
-//   }
+  if (!queryData) {
+    return {
+      props: {
+        notFound: true,
+      },
+    };
+  }
 
-//   return {
-//     props: {
-//       dehydratedState: dehydrate(queryClient),
-//       notFound: false,
-//     },
-//   };
-// };
+  return {
+    props: {
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+      notFound: false,
+    },
+  };
+};
